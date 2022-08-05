1a15df78cb152288806048e8d99d8da846176320
royalrebe <108943414+royalrebe@users.noreply.github.com>
2022-08-05 12:43:45 +0530

    Update __init__.py

--- CARDINGBOT/__init__.py
+++ CARDINGBOT/__init__.py
@@ -8,11 +8,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
-API_ID = int(os.getenv('API_ID', CONFIG['API_ID']))
-API_HASH = int(os.getenv('API_HASH', CONFIG['API_HASH']))
-HEROKU_APP_NAME = int(os.getenv('HEROKU_APP_NAME', CONFIG['HEROKU_APP_NAME']))
-HEROKU_API_KEY = int(os.getenv('HEROKU_API_KEY', CONFIG['HEROKU_API_KEY']))
-TOKEN = int(os.getenv('TOKEN', CONFIG['TOKEN']))
+API_ID = os.getenv('API_ID', CONFIG['API_ID']))
+API_HASH = os.getenv('API_HASH', CONFIG['API_HASH']))
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME', CONFIG['HEROKU_APP_NAME']))
+HEROKU_API_KEY = os.getenv('HEROKU_API_KEY', CONFIG['HEROKU_API_KEY']))
+TOKEN = os.getenv('TOKEN', CONFIG['TOKEN']))
 SUDO_USERS = os.getenv('SUDO_USERS', CONFIG['SUDO_USERS'])
 OWNER_ID = os.getenv('OWNER_ID', CONFIG['OWNER_ID'])
 
